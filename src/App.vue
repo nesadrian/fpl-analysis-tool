a9aee6e0c00fe367c35ca4c3184ea1ff397bedd8
nesadrian <adrian.nesvik@lyse.net>
2020-12-05 13:37:12 +0100

    Add css reset

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,7 @@
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import './assets/reset.css'
 
 export default {
   name: 'App',
@@ -13,14 +14,3 @@ export default {
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
