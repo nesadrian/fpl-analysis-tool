d68298c6b19ce63d262fe95bbee5ef979e0dcbab
nesadrian <adrian.nesvik@lyse.net>
2020-12-07 13:49:23 +0100

    Add loader

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,36 @@
 <template>
-  <Header />
-  <HomePage />
-  <Footer />
+  <template v-if="loading">
+    <div className="loader-container">
+      <MoonLoader />
+    </div>
+  </template>
+  <template v-else>
+    <Header />
+      <HomePage :active="loading"/>
+    <Footer />
+  </template>
 </template>
 
 <script>
 import HomePage from './pages/HomePage.vue'
 import Header from './components/Header'
 import Footer from './components/Footer'
+import MoonLoader from 'vue-spinner/src/MoonLoader.vue'
 import './assets/reset.css'
+import './assets/style.css'
 
 export default {
   name: 'App',
+  data () {
+    return {
+      loading: true
+    }
+  },
   components: {
     HomePage,
     Header,
-    Footer
+    Footer,
+    MoonLoader
   }
 }
 </script>
--- src/api.js
+++ src/api.js
@@ -1,1 +1,7 @@
 const baseURL = 'https://fantasy.premierleague.com/api/'
+
+const getManagerGeneral = (managerId) => fetch(`${baseURL}entry/${managerId}/`).then(res => res.json())
+
+module.exports = {
+  getManagerGeneral
+}
