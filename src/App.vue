edd9afcbe0bf9e466f3332a83aed57357a805b5a
nesadrian <adrian.nesvik@lyse.net>
2020-12-11 16:49:41 +0100

    Refactor App

--- src/App.vue
+++ src/App.vue
@@ -6,44 +6,29 @@
   </template>
   <template v-else>
     <Sidebar />
-    <HomePage :managerId=managerId :dataManagerGeneral="data"/>
+    <HomePage :managerId=managerId />
     <Footer />
   </template>
 </template>
 
 <script>
 import HomePage from './pages/HomePage.vue'
-//import Header from './components/Header'
 import Sidebar from './components/Sidebar'
 import Footer from './components/Footer'
 import MoonLoader from 'vue-spinner/src/MoonLoader.vue'
-import { getDataManagerGeneral } from './api'
 
 export default {
   name: 'App',
   data () {
     return {
-      loading: false,
       managerId: process.env.VUE_APP_MANAGER_ID,
-      data: undefined
     }
   },
   components: {
     HomePage,
     Sidebar,
-    //Header,
     Footer,
     MoonLoader
   },
-  created () {
-    this.fetchData()
-  },
-  methods: {
-    async fetchData () {
-      this.loading = true
-      this.data = await getDataManagerGeneral(this.managerId)
-      this.loading = false
-    }
-  }
 }
 </script>
