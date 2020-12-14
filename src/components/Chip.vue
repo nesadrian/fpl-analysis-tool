918c0d3cf9f34d66fbe6b4385b09957a8fc3148d
nesadrian <adrian.nesvik@lyse.net>
2020-12-14 18:05:43 +0100

    Add chip component

---
+++ src/components/Chip.vue
@@ -0,0 +1,11 @@
+<template>
+  <h1>Chip</h1>
+</template>
+
+<script>
+export default {
+  props: {
+    chip: Object
+  }
+}
+</script>
--- src/components/Chips.vue
+++ src/components/Chips.vue
@@ -1,14 +1,20 @@
 <template>
   <section className="chips-container main__card">
     {{ chips }}
+    <Chip />
   </section>
 </template>
 
 <script>
+import Chip from './Chip'
+
 export default {
   props: {
     chips: Array,
     freeTransfers: Number
+  },
+  components: {
+    Chip
   }
 }
 </script>
