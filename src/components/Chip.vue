d9f51d5c33ea58c4697f5d994f458ae931e196d9
nesadrian <adrian.nesvik@lyse.net>
2020-12-14 22:15:59 +0100

    Add chip styling

--- src/components/Chip.vue
+++ src/components/Chip.vue
@@ -1,15 +1,23 @@
 <template>
   <template v-if="timePlayed">
-    <section className="chip-container">
-      <h1>{{ name }}</h1>
-      <h1>Played</h1>
-      <h1>Gameweek {{ gameweekPlayed }}</h1>
+    <section className="chip-container chip-container--played">
+      <section className="chip__title-container">
+        <h1>{{ name }}</h1>
+      </section>
+      <section className="chip__data-container">
+        <h1>Played</h1>
+        <h1>Gameweek {{ gameweekPlayed }}</h1>
+      </section>
     </section>
   </template>
   <template v-else>
-    <section className="chip-container">
-      <h1>{{ name }}</h1>
-      <h1>Available</h1>
+    <section className="chip-container chip-container--available">
+      <section className="chip__title-container">
+        <h1>{{ name }}</h1>
+      </section>
+      <section className="chip__data-container">
+        <h1>Available</h1>
+      </section>
     </section>
   </template>
 </template>
---
+++ src/components/PageTitle.vue
@@ -0,0 +1,14 @@
+<template>
+  <section>
+    <h1 className="title__text">{{ text }}</h1>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'title',
+  props: {
+    text: String
+  }
+}
+</script>
