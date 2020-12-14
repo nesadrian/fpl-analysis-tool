55eac1f48ddc3cbddc1d068b485d81c8079dc18e
nesadrian <adrian.nesvik@lyse.net>
2020-12-14 21:02:07 +0100

    Add chip conditional rendering

--- src/components/Chip.vue
+++ src/components/Chip.vue
@@ -1,7 +1,17 @@
 <template>
-  <h1>{{ name }}</h1>
-  <h1>{{ timePlayed }}</h1>
-  <h1>{{ gameweekPlayed }}</h1>
+  <template v-if="timePlayed">
+    <section className="chip-container">
+      <h1>{{ name }}</h1>
+      <h1>Played</h1>
+      <h1>Gameweek {{ gameweekPlayed }}</h1>
+    </section>
+  </template>
+  <template v-else>
+    <section className="chip-container">
+      <h1>{{ name }}</h1>
+      <h1>Available</h1>
+    </section>
+  </template>
 </template>
 
 <script>
