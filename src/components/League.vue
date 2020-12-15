c9da60c50e493e53904d3d851a4e33edbb45aeed
nesadrian <adrian.nesvik@lyse.net>
2020-12-15 18:08:47 +0100

    Add league styling

--- src/components/League.vue
+++ src/components/League.vue
@@ -1,17 +1,23 @@
 <template>
   <section className="league-container">
-    <h1 className="card__title">{{ name }}</h1>
-    <h1 className="card__valueDiff card__valueDiff--neutral">{{ formattedRank }}</h1>
-    <h1 className="card__valueDiff card__valueDiff--neutral">{{ formattedLastRank }}</h1>
-    <section v-if="rank - lastRank < 0" className="card__valueDiff-container">
+    <section>
+      <h1 className="card__title">{{ name }}</h1>
+    </section>
+    <section className="league__number">
+      <h1 className="card__valueDiff card__valueDiff--neutral">{{ formattedRank }}</h1>
+    </section>
+    <section className="league__number">
+      <h1 className="card__valueDiff card__valueDiff--neutral">{{ formattedLastRank }}</h1>
+    </section>
+    <section v-if="rank - lastRank < 0" className="card__valueDiff-container league__number">
       <svg class="card__valueDiff--positive card__valueDiff__icon" fill="currentColor" stroke="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M7.247 4.86l-4.796 5.481c-.566.647-.106 1.659.753 1.659h9.592a1 1 0 0 0 .753-1.659l-4.796-5.48a1 1 0 0 0-1.506 0z"/></svg>
       <h1 className="card__valueDiff card__valueDiff--positive">{{ formattedRankDiff }}</h1>
     </section>
-    <section v-else-if="rank - lastRank > 0" className="card__valueDiff-container">
+    <section v-else-if="rank - lastRank > 0" className="card__valueDiff-container league__number">
       <svg class="card__valueDiff--negative card__valueDiff__icon" fill="currentColor" stroke="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M7.247 4.86l-4.796 5.481c-.566.647-.106 1.659.753 1.659h9.592a1 1 0 0 0 .753-1.659l-4.796-5.48a1 1 0 0 0-1.506 0z"/></svg>
       <h1 className="card__valueDiff card__valueDiff--negative">{{ formattedRank }}</h1>
     </section>
-    <section v-else className="card__valueDiff-container">
+    <section v-else className="card__valueDiff-container league__number">
       <h1 className="card__valueDiff card__valueDiff--neutral">{{ '-' }}</h1>
     </section>
   </section>
--- src/components/Leagues.vue
+++ src/components/Leagues.vue
@@ -1,10 +1,10 @@
 <template>
   <section className="leagues-container main__card">
     <section className="league-container">
-      <h1>Name</h1>
-      <h1>Rank</h1>
-      <h1>Last Rank</h1>
-      <h1>Rank Difference</h1>
+      <h1 className="leagues__title">Name</h1>
+      <h1 className="leagues__title">Rank</h1>
+      <h1 className="leagues__title">Previous Rank</h1>
+      <h1 className="leagues__title">Rank Difference</h1>
     </section>
     <li v-for="league in leagues" :key="league.id">
       <League :name="league.name" :rank="league.entry_rank" :lastRank="league.entry_last_rank" />
