a6b822799ac10338e5d32eda15ce67e17645370b
nesadrian <adrian.nesvik@lyse.net>
2020-12-18 20:13:07 +0100

    Add page title and loader

--- src/pages/LeaguePage.vue
+++ src/pages/LeaguePage.vue
@@ -1,9 +1,26 @@
 <template>
-  <h1>League {{ $route.params.id }}</h1>
+  <main v-if="loading" className="loader-container">
+    <MoonLoader />
+  </main>
+  <main v-else className="main-container">
+    <PageTitle text="League" />
+    <h1>League {{ $route.params.id }}</h1>
+  </main>
 </template>
 
 <script>
-export default {
+import PageTitle from '../components/PageTitle'
+import MoonLoader from 'vue-spinner/src/MoonLoader.vue'
 
+export default {
+  data() {
+    return {
+      loading: false,
+    }
+  },
+  components: {
+    PageTitle,
+    MoonLoader
+  }
 }
 </script>
