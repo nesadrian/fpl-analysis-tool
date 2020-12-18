7e9fa1c640561eb9ade47ae00d625504bc7b0fdb
nesadrian <adrian.nesvik@lyse.net>
2020-12-18 13:46:58 +0100

    Add leagues to LeaguesPage

--- src/pages/LeaguesPage.vue
+++ src/pages/LeaguesPage.vue
@@ -1,15 +1,45 @@
 <template>
-  <main className="main-container">
+  <main v-if="loading" className="loader-container">
+    <MoonLoader />
+  </main>
+  <main v-else className="main-container">
     <PageTitle text="Leagues" />
+    <Leagues :leagues="this.dataClassicLeagues"/>
   </main>
 </template>
 
 <script>
 import PageTitle from '../components/PageTitle'
+import Leagues from '../components/Leagues'
+import { sortLeaguesPrivate } from '../helpers'
+import MoonLoader from 'vue-spinner/src/MoonLoader.vue'
 
 export default {
+  data() {
+    return {
+      loading: false,
+    }
+  },
   components: {
-    PageTitle
+    PageTitle,
+    Leagues,
+    MoonLoader
+  },
+  methods: {
+    async fetchData() {
+      this.loading = true
+      await this.$store.dispatch('fetchDataManagerGeneral')
+      this.loading = false
+    }
+  },
+  computed: {
+    dataClassicLeagues() {
+      const leagues = this.$store.getters.getDataManagerGeneral.leagues.classic;
+      if(leagues.length > 0) {
+        return sortLeaguesPrivate(leagues)
+      }
+      return null
+    },
   }
 }
 </script>
