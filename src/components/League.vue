59d7a2d825320012cb83da32d83cfcb6e803dd4f
nesadrian <adrian.nesvik@lyse.net>
2020-12-19 22:31:33 +0100

    Add league member component

--- src/components/League.vue
+++ src/components/League.vue
@@ -1,5 +1,16 @@
 <template>
-  <h1>{{ this.league }}</h1>
+    <section className="leagues-container main__card">
+      <section className="league-container">
+        <h1 className="leagues__title">Name</h1>
+        <h1 className="leagues__title">Rank</h1>
+        <h1 className="leagues__title">Previous Rank</h1>
+        <h1 className="leagues__title">Rank Difference</h1>
+        <h1 className="leagues__title">Points</h1>
+      </section>
+      <li v-for="member in league.standings.results" :key="member.id">
+        <p>{{ member }}</p>
+      </li>
+  </section>
 </template>
 
 <script>
---
+++ src/components/LeagueMember.vue
@@ -0,0 +1,15 @@
+<template>
+  
+</template>
+
+<script>
+export default {
+  props: {
+    teamName: String,
+    managerName: String,
+    rank: Number,
+    lastRank: Number,
+    points: Number
+  }
+}
+</script>
