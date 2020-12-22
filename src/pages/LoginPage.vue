5dca98699c9239d5e3a7ed464e7e2d4323de6494
nesadrian <adrian.nesvik@lyse.net>
2020-12-23 00:45:32 +0100

    Add submit handler

--- src/pages/LoginPage.vue
+++ src/pages/LoginPage.vue
@@ -1,14 +1,26 @@
 <template>
   <main class="main-container main-container--login">
-    <form>
-      <input type="text" placeholder="Type manager id..."/>
+    <form @submit="handleSubmit">
+      <input v-model="inputText" type="text" placeholder="Type manager id..."/>
       <button type="submit">Login</button>
     </form>
   </main>
 </template>
 
 <script>
-export default {
+import { isValidManagerId } from '../helpers'
 
+export default {
+  data() {
+    return {
+      inputText: ''
+    }
+  },
+  methods: {
+    handleSubmit(e) {
+      isValidManagerId(this.inputText)
+      e.preventDefault()
+    }
+  }
 }
 </script>
